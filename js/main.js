cdb1d63125057534786b0ffb12612effff7fc828
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2024-12-26 16:45:17 +0300

    Правит функции получения случайных чисел.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-function getRandomInteger(min, max) {
-  if (min < 0 || max <= min) {
-    throw 'Диапазон должен быть положительным и не может быть равен нулю!';
-  }
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
-}
+  return Math.floor(result);
+};
 
 // Чтобы ESLint не ругался на неиспользуемые функции, временно вызовите их по одному разу после объявления.
-getRandomInteger(0, 10);
+getRandomPositiveInteger(0, 10);
 
-function getRandomFloat(min, max, precision) {
-  if (min < 0 || max <= min) {
-    throw 'Диапазон должен быть положительным и не может быть равен нулю!';
-  }
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
 
-  return (Math.random() * (max - min) + min).toFixed(precision); // Максимум и минимум включаются
-}
+  return +result.toFixed(digits);
+};
 
 // Чтобы ESLint не ругался на неиспользуемые функции, временно вызовите их по одному разу после объявления.
-getRandomFloat(1, 2, 3);
+getRandomPositiveFloat(1, 2, 4);
